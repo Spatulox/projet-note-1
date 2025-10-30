8b10b50d52d75bd266677d600bb703ba14cf5201
Spatulox <M78stormtrooper@laposte.net>
2025-10-30 15:20:30 +0100

    add chance

--- index.test.ts
+++ index.test.ts
@@ -9,6 +9,7 @@ describe("Figure Test", () => {
         [[1, 1, 1, 4, 4] as Rolls, Figure.Full],            // Full
         [[1, 2, 3, 4, 5] as Rolls, Figure["Grande Suite"]], // Grande Suite
         [[5, 5, 5, 5, 5] as Rolls, Figure.YAMS],            // YAMS
+        [[5, 4, 2, 3, 1] as Rolls, Figure.Chance],          // Chance
     ])("should send %i to %s", (num, expected) => {
         expect(Figure[new Game(num).state()]).toBe(Figure[expected]);
     });
